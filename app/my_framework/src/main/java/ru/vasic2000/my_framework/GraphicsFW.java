85d92d7f4be74a359db05b9a92238d2420b06e2b
vasic2000 <vasic2000@yandex.ru>
2021-07-13 21:53:40 +0300

    методы рисования

--- app/my_framework/src/main/java/ru/vasic2000/my_framework/GraphicsFW.java
+++ app/my_framework/src/main/java/ru/vasic2000/my_framework/GraphicsFW.java
@@ -4,6 +4,7 @@ import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Typeface;
 
 class GraphicsFW {
     private AssetManager assetManagerGame;
@@ -17,5 +18,30 @@ class GraphicsFW {
         this.frameBufferGame = frameBufferGame;
         this.canvasGame = new Canvas(frameBufferGame);
         this.paintGame = new Paint();
+    }
+
+    public void clearScene(int colorRGB) {
+        canvasGame.drawRGB(colorRGB, colorRGB, colorRGB);
+    }
+
+    public void drawPixel(int x, int y, int color) {
+        paintGame.setColor(color);
+        canvasGame.drawPoint(x, y, paintGame);
+    }
+
+    public void drawLine(int startX, int startY, int stopX, int stopY, int color) {
+        paintGame.setColor(color);
+        canvasGame.drawLine(startX, startY, stopX, stopY, paintGame);
+    }
+
+    public void drawText(String text, int x, int y, int color, int textSize, Typeface font) {
+        paintGame.setColor(color);
+        paintGame.setTextSize(textSize);
+        paintGame.setTypeface(font);
+        canvasGame.drawText(text, x, y, paintGame);
+    }
+
+    public void drawTexture(Bitmap textureGame, int x, int y) {
+        canvasGame.drawBitmap(textureGame, x, y, null);
     }
 }
