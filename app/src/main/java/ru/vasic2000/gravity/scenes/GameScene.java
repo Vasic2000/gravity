c0740905a20c4c39e1ea8fc02dac9fe360277636
vasic2000 <vasic2000@yandex.ru>
2021-07-16 22:35:56 +0300

    Game Scene part 1

--- app/src/main/java/ru/vasic2000/gravity/scenes/GameScene.java
+++ app/src/main/java/ru/vasic2000/gravity/scenes/GameScene.java
@@ -2,25 +2,88 @@ package ru.vasic2000.gravity.scenes;
 
 import android.graphics.Color;
 
+import ru.vasic2000.gravity.R;
 import ru.vasic2000.my_framework.CoreFW;
 import ru.vasic2000.my_framework.SceneFW;
 
 public class GameScene extends SceneFW {
 
+    enum GameState {
+        READY,
+        RUNING,
+        PAUSE,
+        GAMEOVER
+    }
+
+    GameState gameState;
+
     public GameScene(CoreFW coreFW) {
         super(coreFW);
+        gameState = GameState.READY;
     }
 
     @Override
     public void update() {
-
+        if(gameState == GameState.READY) {
+            updateStateReady();
+        }
+        if(gameState == GameState.RUNING) {
+            updateStateRuning();
+        }
+        if(gameState == GameState.PAUSE) {
+            updateStatePause();
+        }
+        if(gameState == GameState.GAMEOVER) {
+            updateStateGameOver();
+        }
     }
 
     @Override
     public void drawing() {
+//        graficsFW.drawText("Game Scene",
+//                100, 150, Color.GREEN, 60,null);
+
+        if(gameState == GameState.READY) {
+            drawingGameReady();
+        }
+        if(gameState == GameState.RUNING) {
+            drawingGameRuning();
+        }
+        if(gameState == GameState.PAUSE) {
+            drawingGamePause();
+        }
+        if(gameState == GameState.GAMEOVER) {
+            drawingGameGameOver();
+        }
+    }
+
+    private void drawingGameReady() {
         graficsFW.clearScene(Color.BLACK);
-        graficsFW.drawText("Game Scene",
-                100, 150, Color.GREEN, 60,null);
+        graficsFW.drawText(coreFW.getString(R.string.txt_gameScene_stateReady_ready),
+                250, 300, Color.WHITE, 60, null);
+    }
+    private void updateStateReady() {
+        if(coreFW.getTouchListenerFW().getTuchUp(0, sceneHeight, sceneWidth, sceneHeight)) {
+            gameState = GameState.RUNING;
+        }
+    }
+
+    private void drawingGameRuning() {
+        graficsFW.clearScene(Color.BLACK);
+        graficsFW.drawText("Сцена игры",
+                250, 300, Color.WHITE, 60, null);
+    }
+    private void updateStateRuning() {
+    }
+
+    private void drawingGamePause() {
+    }
+    private void updateStatePause() {
+    }
+
+    private void drawingGameGameOver() {
+    }
+    private void updateStateGameOver() {
     }
 
     @Override
