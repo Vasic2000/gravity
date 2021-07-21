ee63aa1ca49693f51c99d37dadb5918ea4f4ab4d
Vasic2000 <vasic2000@yandex.ru>
2021-07-21 21:57:40 +0300

    LoaderAssets

--- app/src/main/java/ru/vasic2000/gravity/classes/LoaderAssets.java
+++ app/src/main/java/ru/vasic2000/gravity/classes/LoaderAssets.java
@@ -1,5 +1,28 @@
 package ru.vasic2000.gravity.classes;
 
+import ru.vasic2000.my_framework.CoreFW;
+import ru.vasic2000.my_framework.GraphicsFW;
+import ru.vasic2000.my_framework.utils.UtilResourse;
+
 public class LoaderAssets {
+    public LoaderAssets(CoreFW coreFW, GraphicsFW graphicsFW) {
+        loadTexture(graphicsFW);
+        loadSpritePlayer(graphicsFW);
+    }
+
+    private void loadTexture(GraphicsFW graphicsFW) {
+        UtilResourse.textureAtlas = graphicsFW.newTexture("texture_atlas.png");
+    }
+
+    private void loadSpritePlayer(GraphicsFW graphicsFW) {
+        UtilResourse.spritePlayer.add(graphicsFW.newSprite(UtilResourse.textureAtlas,
+                0, 0, 64, 64));
+        UtilResourse.spritePlayer.add(graphicsFW.newSprite(UtilResourse.textureAtlas,
+                64, 0, 64, 64));
+        UtilResourse.spritePlayer.add(graphicsFW.newSprite(UtilResourse.textureAtlas,
+                128, 0, 64, 64));
+        UtilResourse.spritePlayer.add(graphicsFW.newSprite(UtilResourse.textureAtlas,
+                192, 0, 64, 64));
+    }
 
 }
