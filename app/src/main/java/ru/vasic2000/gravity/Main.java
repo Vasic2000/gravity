cf88f238b850a4d8cd26e445d326db18720d82bf
vasic2000 <vasic2000@yandex.ru>
2021-08-12 18:37:03 +0300

    Не до конца.

--- app/src/main/java/ru/vasic2000/gravity/Main.java
+++ app/src/main/java/ru/vasic2000/gravity/Main.java
@@ -1,13 +1,17 @@
 package ru.vasic2000.gravity;
 
-import ru.vasic2000.gravity.scenes.LoaderResourcesScene;
+import ru.vasic2000.gravity.classes.LoaderAssets;
+import ru.vasic2000.gravity.scenes.MainMenuScene;
 import ru.vasic2000.my_framework.CoreFW;
 import ru.vasic2000.my_framework.SceneFW;
 
 public class Main extends CoreFW {
 
     public SceneFW getStartScene() {
-        return new LoaderResourcesScene(this);
+        //TODO other Thread for loaders
+        LoaderAssets loaderAssets = new LoaderAssets(
+                this, this.getGraphicsFW());
+        return new MainMenuScene(this);
     }
 
 }
